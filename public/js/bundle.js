0fd808b01f9c729f2a5784ef2a0cb03d3613fcef
Nsanzimana Gilbert <nsanzimanagilbal@gmail.com>
2022-07-25 13:01:03 +0300

    modified API URLs

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -34110,7 +34110,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59640" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60118" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
